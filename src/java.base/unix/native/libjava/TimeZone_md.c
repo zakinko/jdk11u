357953688e92b29a3ed638519766c2b756e97651
Showta Ishizaki <zakinko@snowrabbit.org>
2026-09-15 05:20:18 +0900

    Let the BSDs reach getPlatformTimeZoneID
    The guard over the block that defines it names __linux__ and MACOSX, so on
    a BSD neither that definition nor the _AIX one is compiled, and the call in
    findJavaTZ_md() is left undeclared.  Say _ALLBSD_SOURCE, which is what 28
    does.
    
    Mine: resolving the backport I kept this file's HEAD on both hunks, judging
    them a comment on an #endif, and took the substantive line with it.
    
    (cherry picked from commit a9519de56da0646e2c01446562b93b7e04f8ae9c)
    (cherry picked from commit c40216739321d851b9d28fa1beaeb4268b138ce5)
    (cherry picked from commit 59b6b6d7becca8003fbb8abf2b96c18ecd7419e0)

--- src/java.base/unix/native/libjava/TimeZone_md.c
+++ src/java.base/unix/native/libjava/TimeZone_md.c
@@ -79,7 +79,7 @@ static const char popularZones[][4] = {"UTC", "GMT"};
 static const char *ETC_ENVIRONMENT_FILE = "/etc/environment";
 #endif
 
-#if defined(__linux__) || defined(MACOSX) || defined(__solaris__)
+#if defined(__linux__) || defined(_ALLBSD_SOURCE) || defined(__solaris__)
 
 /*
  * Returns a pointer to the zone ID portion of the given zoneinfo file
@@ -242,7 +242,7 @@ isFileIdentical(char *buf, size_t size, char *pathname)
     return possibleMatch;
 }
 
-#if defined(__linux__) || defined(MACOSX)
+#if defined(__linux__) || defined(_ALLBSD_SOURCE)
 
 /*
  * Performs Linux specific mapping and returns a zone ID
